c0e84550804adb4eb65d84e9b16444845e5762c5
GumarGarifullyn <118556134+GumarGarifullyn@users.noreply.github.com>
2023-03-10 16:45:56 +0300

    add task 16

--- Домашнее задание/ДЗ_3/Задача 16.py
+++ Домашнее задание/ДЗ_3/Задача 16.py
@@ -3,4 +3,14 @@
 # натуральное число N – количество элементов в массиве. В последующих
 # строках записаны N целых чисел Ai
 # . Последняя строка содержит число X
+import random
 
+num = int(input("Введите число от 0 до 5, повторяющееся в списке: "))
+lenght_list = int(input('Введите длину листа: '))
+my_list = [random.randint(0, 5) for _ in range(lenght_list)]
+print(my_list)
+count = 0
+for elem in my_list:
+    if elem == num:
+        count += 1
+print(count)
